c30a423b62ab96a9c00224d64c067d2008c07550
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-15 20:44:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-  puts programmer_hash["grace_hopper"]
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,8 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     
-  puts programmer_hash["alan_kay"]["known_for"]
+  programmer_hash[:alan_kay][known_for]
 end
 
 def dennis_ritchies_language
@@ -57,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     puts programmer_hash["dennis_ritchie"]["languages"]
+     programmer_hash[:dennis_ritchie][languages]
 end
 
 def adding_matz
@@ -83,11 +81,10 @@ def adding_matz
         }
      }
 
-  programmer_hash["yukihiro_matsumoto"] = {
+  programmer_hash[:yukihiro_matsumoto] = {
       :known_for => "Ruby",
       :languages => ["LISP, C"]
   }
-  puts programmer_hash
 end
 
 def changing_alan
@@ -108,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash["alan_kay"]["known_for"] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
@@ -129,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash["dennis_ritchie"]["languages"]= "C, Assembly"
+  programmer_hash[:dennis_ritchie][:languages]= "C, Assembly"
 end
